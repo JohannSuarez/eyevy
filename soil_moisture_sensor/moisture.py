d90ac57ae0c9b83f98e0b710d1e1520cc8257e4d
Johann Suarez <johann.suarez92@gmail.com>
2022-04-06 09:39:53 +0100

    Moisture sensor test code

--- soil_moisture_sensor/moisture.py
+++ soil_moisture_sensor/moisture.py
@@ -1,4 +1,14 @@
+"""
+To get this sensor module working, you need CircuitPython installed.
+https://learn.adafruit.com/circuitpython-on-raspberrypi-linux/installing-circuitpython-on-raspberry-pi
+
+After that, the sample code from here should work:
+https://learn.adafruit.com/circuitpython-on-raspberrypi-linux/installing-circuitpython-on-raspberry-pi
+"""
+
 import time
+import requests
+import statistics
 import board
 
 from adafruit_seesaw.seesaw import Seesaw
@@ -7,20 +17,48 @@ i2c_bus = board.I2C()
 
 ss = Seesaw(i2c_bus, addr=0x36)
 
-'''
-To  get this sensor module working, you need CircuitPython installed.
-https://learn.adafruit.com/circuitpython-on-raspberrypi-linux/installing-circuitpython-on-raspberry-pi
+def grab_temp() -> float:
+    """
+    @desc Read temperature (Accurate by +-2*C)
+    """
+    return ss.get_temp()
 
-After that, the sample code should work here
-https://learn.adafruit.com/circuitpython-on-raspberrypi-linux/installing-circuitpython-on-raspberry-pi
-'''
+def grab_moisture() -> float:
+    """
+    @desc Read moisture (Always recalibrate everytime you replant this sensor)
+    """
+    return ss.moisture_read()
 
-while True:
-	# read moisture level through capacitive touch pad
-	touch = ss.moisture_read()
-	
-	# read temperrature from the temperature sensor
-	temp = ss.get_temp()
 
-	print(f"temp: {temp}  moisture: {touch}")
-	time.sleep(1)
+def main():
+
+    url: str = 'https://discord.com/api/webhooks/945755701760909352/jW8tKjVsDUxX4_fLzgX8-WO6SIyu5ozMz2cVxKfi4oCpd9IOfUHR5PO36mk-QCcuBz-U'
+
+    data: float | str
+
+    headers: dict[str, str] = {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            }
+
+    reading_collection: List[float] = []
+    for i in range(10):
+        moisture: float = grab_moisture()
+        # print(moisture)
+        reading_collection.append(moisture)
+        time.sleep(1)
+    
+    try:
+        data = statistics.fmean(reading_collection)
+        # print(f"Test Soil moisture reading: {data}")
+    except Exception as e:
+        data = "Reading failed."
+
+    json_data = {
+        'content': f"Soil Moisture Reading: {str(data)}"
+    }
+
+    response = requests.post(url, headers=headers, json=json_data)
+
+if __name__ == "__main__":
+    main()
